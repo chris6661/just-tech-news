018171be1e4d2461eae684b50f804c0a0abcfb11
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-03-24 00:58:16 -0500

    added in home page for front end of just tech news

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
-
+//for handlebars 
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//for handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
